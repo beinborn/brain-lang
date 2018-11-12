Harry pipeline is now working but still very hacky. Explained variance is a ridiculous negative value. There is still a lot wrong.

computational_model/combine_representations.py
@@ -1,11 +1,12 @@
 import numpy as np
 
+
 # Variant a: combine word embeddings into a sentence embedding
 # Variant b: combine sentence embeddings into a story embedding (for Kaplan data)
 # TODO: a and b probably require different methods
 
-#def combine_wordToSentence(stimulus, embedding):
- #   # TODO Samira: add your weighted averaging methods here
+# def combine_wordToSentence(stimulus, embedding):
+#   # TODO Samira: add your weighted averaging methods here
 
 def combine_sentenceToStory(stimulus, embedding):
     if not len(stimulus) == len(embedding):
@@ -16,6 +17,7 @@ def combine_sentenceToStory(stimulus, embedding):
     for e in embedding:
         np.concatenate(concatenated_embedding, e)
     return concatenated_embedding
+
 
 # So far, this method only works for the Harry Potter data and for elmo embeddings.
 # It is absolutely not nice.
@@ -42,9 +44,9 @@ def get_stimulus_embeddings(sentences, sentence_embeddings, scans):
                     token_id = 0
 
                 # Make sure everything is aligned correctly
-                print("Sentence id: "+ str(sentence_id))
-                print("Token id: " + str(token_id))
-                print("Stimulus token: " + token)
+                # print("Sentence id: "+ str(sentence_id))
+                # print("Token id: " + str(token_id))
+                # print("Stimulus token: " + token)
                 if not (token.strip() == sentences[sentence_id][token_id].strip()):
                     print("Mismatch: ")
                     print(token)
@@ -64,18 +66,40 @@ def get_stimulus_embeddings(sentences, sentence_embeddings, scans):
         # stimulus_embeddings.append(np.average(np.matrix(token_embeddings)))
         else:
             stimulus_embeddings.append([])
+
     return stimulus_embeddings
 
     # Very naive approach to the delay, just move the embeddings two timesteps down and skip the last two scans
     # TODO Discuss implementation of better methods with Samira!
-def add_delay(timesteps, scans, embeddings):
-    seed = 42
-    empty_embedding = np.random.rand(embeddings[0].shape)
 
-    embeddings = [[empty_embedding] * timesteps] + embeddings
-    scans = scans[0:(len(scans) - 2)]
+
+def add_delay(timesteps, scans, embeddings):
+    # Very naive implementation of the delay.
+    # I just add random fixation embeddings to the beginning according to the number of timesteps.
+    #  todo: dicusss
+
+    np.random.seed(42)
+    embedding_dim = len(max(embeddings, key=len))
+    fixation_embedding = np.random.rand(embedding_dim, )
+
+    for i in range(0, len(embeddings)):
+        if len(embeddings[i]) == 0:
+            embeddings[i] = fixation_embedding
+
+    # Add fixation embeddings to the beginning
+    initial_embeddings = []
+    for step in range(0, timesteps):
+        initial_embeddings.append(fixation_embedding)
+
+    embeddings = initial_embeddings + embeddings
+
+    # Remove last two embeddings from end
+    embeddings = embeddings[0:(len(embeddings) - timesteps)]
+
     if not len(embeddings) == len(scans):
         print("Embeddings and scans do not have the same length")
         print(len(embeddings))
         print(len(scans))
-    return embeddings, scans
+    else:
+        print("All fine.")
+    return scans, embeddings