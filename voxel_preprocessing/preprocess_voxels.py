c9c8ed32db010fc812650cf036789f12f13c1ab1
beinborn <lisa.beinborn@gmx.de>
2018-11-14 14:51:06 +0100

    adjusted preprocess_voxels to be inline with Samira

--- voxel_preprocessing/preprocess_voxels.py
+++ voxel_preprocessing/preprocess_voxels.py
@@ -1,12 +1,12 @@
 import nilearn.signal
 from scipy import stats
 import numpy as np
-def clean(data):
-    # check data format
-    return nilearn.signal.clean(data, sessions=None,
-                         detrend=True, standardize=True,
+
+def detrend(timeseries_datapoints, t_r, standardize=False):
+    return nilearn.signal.clean(timeseries_datapoints, sessions=None,
+                         detrend=True, standardize=standardize,
                          confounds=None, low_pass=None,
-                         high_pass=0.005, t_r=2.0, ensure_finite=False)
+                         high_pass=0.005, t_r=t_r, ensure_finite=False)
 
 
 # Transform voxel values into z-scores. (x-mean)/stdev
@@ -25,3 +25,16 @@ def select(data):
     print("After eliminating columns" + str(adjusted_data.shape))
     return adjusted_data
 
+
+def minus_average_resting_states(timeseries_datapoints, brain_states_with_no_stimuli):
+  """
+  :param timeseries_datapoints:
+  :param brain_states_with_no_stimuli:
+  :return:
+  """
+
+  # For now we simply normalize by minusing the avg resting state.
+  average_brain_state_with_no_stimuli = np.mean(brain_states_with_no_stimuli, axis=-1)
+  timeseries_datapoints = timeseries_datapoints - average_brain_state_with_no_stimuli
+
+  return timeseries_datapoints
